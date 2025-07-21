b2824be136fb3206e10140d55aa2182b9b3e9704
Alexander Farber <farber72@outlook.de>
2025-07-21 13:30:13 +0200

    Extract localization strings to separate files

--- checkered-board-buttons/vite-plugin-localize.js
+++ checkered-board-buttons/vite-plugin-localize.js
@@ -2,83 +2,14 @@
 
 import fs from "fs";
 import path from "path";
+import en from "./locales/en.js";
+import de from "./locales/de.js";
+import fr from "./locales/fr.js";
 
 const localizedStrings = {
-  en: {
-    ___YES___: "Yes",
-    ___NO___: "No",
-    ___CANCEL___: "Cancel",
-    ___GAME___: "Game",
-    ___NEW_GAME___: "New game",
-    ___TWO_LETTERS___: "2 letters",
-    ___THREE_LETTERS___: "3 letters",
-    ___RARE_LETTER_1___: "Letter Q",
-    ___RARE_LETTER_2___: "Letter X",
-    ___YOUR_TURN___: "YOUR TURN",
-    ___HIS_TURN___: "WAITING",
-    ___ARCHIVE___: "ARCHIVE",
-    ___ZERO_GAMES___: "NO GAMES",
-    ___FULL_SCREEN___: "Full screen",
-    ___SWAP___: "Swap",
-    ___SKIP___: "Skip",
-    ___RESIGN___: "Resign",
-    ___PILE___: "Pile: 86",
-    ___MOVES_HISTORY___: "Moves history",
-    ___SETTINGS___: "Settings",
-    ___SHARE___: "Share",
-    ___PLAY___: "Play",
-    ___SHUFFLE___: "Shuffle"
-  },
-  de: {
-    ___YES___: "Ja",
-    ___NO___: "Nein",
-    ___CANCEL___: "Abbrechen",
-    ___GAME___: "Spiel",
-    ___NEW_GAME___: "Neues Spiel",
-    ___TWO_LETTERS___: "2 Buchstaben",
-    ___THREE_LETTERS___: "3 Buchstaben",
-    ___RARE_LETTER_1___: "Buchstabe Q",
-    ___RARE_LETTER_2___: "Buchstabe Y",
-    ___YOUR_TURN___: "IHR ZUG",
-    ___HIS_TURN___: "WARTEN",
-    ___ARCHIVE___: "ARCHIV",
-    ___ZERO_GAMES___: "KEINE SPIELE",
-    ___FULL_SCREEN___: "Vollbild",
-    ___SWAP___: "Wechseln",
-    ___SKIP___: "Aussetzen",
-    ___RESIGN___: "Aufgeben",
-    ___PILE___: "Beutel: 86",
-    ___MOVES_HISTORY___: "Züge",
-    ___SETTINGS___: "Einstellungen",
-    ___SHARE___: "Teilen",
-    ___PLAY___: "Spielen",
-    ___SHUFFLE___: "Mischen"
-  },
-  fr: {
-    ___YES___: "Oui",
-    ___NO___: "Non",
-    ___CANCEL___: "Annuler",
-    ___GAME___: "Jeu",
-    ___NEW_GAME___: "Nouveau jeu",
-    ___TWO_LETTERS___: "2 lettres",
-    ___THREE_LETTERS___: "3 lettres",
-    ___RARE_LETTER_1___: "Lettre K",
-    ___RARE_LETTER_2___: "Lettre W",
-    ___YOUR_TURN___: "VOTRE TOUR",
-    ___HIS_TURN___: "EN ATTENDANT",
-    ___ARCHIVE___: "ARCHIVE",
-    ___ZERO_GAMES___: "PAS DE JEUX",
-    ___FULL_SCREEN___: "Plein écran",
-    ___SWAP___: "Échange",
-    ___SKIP___: "Sauter",
-    ___RESIGN___: "Démissionner",
-    ___PILE___: "Pile: 86",
-    ___MOVES_HISTORY___: "Déplacer l'histoire",
-    ___SETTINGS___: "Réglages",
-    ___SHARE___: "Partager",
-    ___PLAY___: "Jouer à",
-    ___SHUFFLE___: "Mélange"
-  }
+  en,
+  de,
+  fr
 };
 
 function replacePlacesholders(src, lang) {
